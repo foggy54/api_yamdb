5297039fbf07a35f95d0fd3c9956bd78f1cd2643
Ivan Plakunov <its-me-again@yandex.ru>
2022-07-13 12:07:58 +0300

    Feature: Rertieve authentication token is implemented

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -14,5 +14,11 @@ class EmailRegistration(serializers.Serializer):
     email = serializers.EmailField()
     username = serializers.CharField(max_length=50)
     
+
+class LoginUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    access_code = serializers.CharField(write_only=True)
+    
+    
         
 
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,22 +1,26 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import SimpleRouter
-from .views import UserSignUpViewSet, EmailRegistrationView
+from .views import (
+    UserSignUpViewSet,
+    EmailRegistrationView,
+    RetrieveAccessToken,
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 
-
-router = SimpleRouter() #не знаю, нужно ли это тут, но пока добавил
+router = SimpleRouter()  # не знаю, нужно ли это тут, но пока добавил
 # router.register('auth/signup', UserSignUpViewSet, basename='auth-signup')
 #
 
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/signup/', EmailRegistrationView.as_view()),
-    
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'auth/token/', RetrieveAccessToken.as_view(), name='token_obtain_pair'
+    ),
+    # path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
