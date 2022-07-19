99ec33e6ea9849f091b47f68c8eac13008112d46
Ivan Plakunov <its-me-again@yandex.ru>
2022-07-19 18:56:44 +0300

    Permissions py fixed

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,16 +7,15 @@ class IsAdmin(BasePermission):
     """
 
     def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return request.user.is_admin or request.user.is_superuser
-        else:
-            return False
+
+        return request.user.is_authenticated and (
+            request.user.is_admin or request.user.is_superuser
+        )
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated:
-            return request.user.is_admin or request.user.is_superuser
-        else:
-            return False
+        return request.user.is_authenticated and (
+            request.user.is_admin or request.user.is_superuser
+        )
 
 
 class IsSelf(BasePermission):
